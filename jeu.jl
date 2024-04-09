89c715d78c14689d7f52ab37dafa233657b89bb7
LinaLH <121266302+LinaLH@users.noreply.github.com>
2024-04-09 19:35:57 +0200

    Améliorations

--- jeu.jl
+++ jeu.jl
@@ -16,18 +16,32 @@ function resoudre_et_dessiner_grille_pointillee(grille_dim, contraintes_lignes, 
     optimize!(model)
 
     if termination_status(model) == MOI.OPTIMAL
+        # Afficher les contraintes des colonnes au-dessus de la grille
+        print("   ")  # Espace pour aligner avec les colonnes de la grille
+        for j in 1:grille_dim
+            print("  $(contraintes_colonnes[j]) ")
+        end
+        println()
+
         # Dessiner la grille pointillée avec des cases sélectionnées
         for i in 0:grille_dim
-            # Ligne horizontale en pointillés
-            println("+" * join(repeat(["--+"], grille_dim)))
+            if i > 0
+                # Afficher les contraintes des lignes à gauche de la grille
+                print("$(contraintes_lignes[i]) ")
+            else
+                print("  ")
+            end
             
-            # Contenu de la grille et ligne verticale en pointillés si i < grille_dim
-            if i < grille_dim
+            if i == 0
+                println("+" * join(repeat(["---+"], grille_dim)))
+            else
+                # Contenu de la grille et ligne verticale en pointillés
                 for j in 1:grille_dim
-                    fill_char = value(x[i+1, j]) > 0.5 ? " ◼️" : "  "
+                    fill_char = value(x[i, j]) > 0.5 ? " ◼️ " : "   "
                     print("|" * fill_char)
                 end
                 println("|")
+                println("  +" * join(repeat(["---+"], grille_dim)))  # Ligne horizontale en pointillés
             end
         end
     else
@@ -35,11 +49,37 @@ function resoudre_et_dessiner_grille_pointillee(grille_dim, contraintes_lignes, 
     end
 end
 
-
 # Dimensions de la grille et contraintes
+
+#Exemple
 grille_dim = 5
 contraintes_lignes = [5, 3, 3, 5, 2]  # Exemple de contraintes pour les lignes
 contraintes_colonnes = [5, 5, 2, 2, 4]  # Exemple de contraintes pour les colonnes
 
-# Appel de la fonction pour dessiner une grille pointillee
+#Jeu 1
+#grille_dim = 5
+#contraintes_lignes = [4, 5, 3, 3, 3] 
+#contraintes_colonnes = [3, 4, 3, 3, 5]
+
+#Jeu 2
+#grille_dim = 5
+#contraintes_lignes = [3, 5, 2, 5, 3]
+#contraintes_colonnes = [3, 2, 4, 5, 4]
+
+#Jeu 3
+#grille_dim = 5
+#contraintes_lignes = [3, 4, 5, 4, 2]
+#contraintes_colonnes = [4, 4, 3, 3, 4] 
+
+#Jeu 4
+#grille_dim = 6
+#contraintes_lignes = [3, 4, 3, 2, 6, 2]
+#contraintes_colonnes = [4, 4, 2, 3, 2, 5]
+
+#Jeu 5
+#grille_dim = 6
+#contraintes_lignes = [4, 5, 5, 2, 5, 5]
+#contraintes_colonnes = [4, 5, 4, 5, 5, 3]
+
+# Appel de la fonction pour dessiner une grille pointillée
 resoudre_et_dessiner_grille_pointillee(grille_dim, contraintes_lignes, contraintes_colonnes)
